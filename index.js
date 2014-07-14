e90d06d4d98861920c0e7f3b749a7812ff0673aa
adam souzis <adam@souzis.com>
2014-07-14 14:43:05 -0700

    fix running base app directly

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ module.exports = require('./lib/app');
 
 // check to see if we're the main module (i.e. run directly, not require()'d)
 if (require.main === module) {
-  module.exports.createApp().start();
+  module.exports.createApp(__dirname).start();
 }
