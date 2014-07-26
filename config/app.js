7bc75ebb558d3f8f7462a277cdbe204ca872a56a
adam souzis <adam@souzis.com>
2014-07-26 08:43:29 -0700

    remove data migration functionality and keystone dependency

--- config/app.js
+++ config/app.js
@@ -1,7 +1,6 @@
 module.exports = {
     dburl: "mongodb://127.0.0.1:27017/ocdemo",
     port: 3000,
-    autoUpdates: false,
     cacheviews: false,
     //cookie_secret: 'your secret here',
     //sessionfactory: function() {return {store: new FileStore()}},
--- test/config/app.js
+++ test/config/app.js
@@ -1,7 +1,6 @@
 module.exports = {
     dburl: "mongodb://127.0.0.1:27017/ocdemo-unittest",
     port: null, //don't specify a port so we can let the OS choose a free one
-    autoUpdates: false,
     cookie_secret: "unit tests rule!",
     request_logger: null,
 }
