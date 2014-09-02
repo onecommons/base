b73ef3d98f8d0ef7d96aebd0d05ecce6127aff30
adam souzis <adam@souzis.com>
2014-09-01 17:58:12 -0700

    add to ability to specify config overrides in the config loader

--- test/configtest.js
+++ test/configtest.js
@@ -1,11 +1,28 @@
 var assert = require('assert'),
-  loadConfig = require('../lib/config')();
+  configloader = require('../lib/config');
 
 describe("config", function() {
+  it("should merge global overrides", function() {
+    var loadConfig = configloader({
+      configtest: {
+        inboth: "overridden",
+        overrideonly: true
+      }
+    });
+    assert(process.env.NODE_ENV==='test'); //NODE_ENV=test will always be defined
+    var config = loadConfig("configtest");
+    assert(config.defaultonly === true);
+    assert(config.inboth === "overridden");
+    assert(config.overrideonly === true);
+  });
+
   it("should merge defaults and local", function() {
+    var loadConfig = configloader(); //use default paths
     assert(process.env.NODE_ENV==='test'); //NODE_ENV=test will always be defined
     var config = loadConfig("configtest");
     assert(config.defaultonly === true);
     assert(config.inboth === "local");
+    assert(!config.overrideonly);
   });
+
 });
