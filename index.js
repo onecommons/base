b7486bb6875404e3830b13e38b8b2566e22f94ef
adam souzis <adam@souzis.com>
2014-10-21 10:45:46 -0700

    transparently use BASESRC env variable

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-module.exports = require('./lib/app');
+if (process.env.BASESRC) {
+  var path = require('path');
+  module.exports = require(path.join(process.env.BASESRC, 'lib/app'));
+} else {
+  module.exports = require('./lib/app');
+}
 
 // check to see if we're the main module (i.e. run directly, not require()'d)
 if (require.main === module) {
