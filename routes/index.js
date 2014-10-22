57dc68d012dee318483b76e343ffdd6a60d64521
adam souzis <adam@souzis.com>
2014-10-22 06:17:48 -0700

    remove ui bits related to payment processing

--- routes/index.js
+++ routes/index.js
@@ -39,22 +39,9 @@ module.exports = function(app, passport) {
                         get:  login.forgotToken,
                         post: login.forgotTokenPost },
 
-    setupPaymentPlan: { path: 'profile/setup-payment-plan',
-                        get:  [ utils.isLoggedIn, utils.renderer('setup-payment-plan')],
-                        post: [ utils.isLoggedIn, payments.setupPaymentPlanPost] },
-
-    fundCampaign:     { path: 'fund-campaign/:id',
-                        get:  [ utils.isLoggedIn, payments.fundCampaignGet],
-                        post: [ utils.isLoggedIn, payments.fundCampaignPost] },
-
-    fundCampaignNoId: { path: 'fund-campaign',
-                        get:  [ utils.isLoggedIn, payments.fundCampaignGet] },
-
     datarequest:      { post: [utils.isLoggedIn, datarequest]},
 
     profile:          [ utils.isLoggedIn, login.profile],
-
-    userTransactions: [ 'profile/transactions', utils.isRecentlyLoggedIn, profile.transactionHistory],
 
     jswig:            ['jswig/*', jswig(app)],
   };
