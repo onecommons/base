6264bb7d846099c85b8366d218a898ef8e3a0fc0
adam souzis <adam@souzis.com>
2014-11-01 08:49:20 -0700

    add helper functions for promises, error responses, and database object instantiation

--- models/index.js
+++ models/index.js
@@ -1,5 +1,8 @@
-var exportModel = require('../lib/utils').exportModel;
+var utils = require('../lib/utils');
 var _ = require('underscore');
+var Promise = require('promise');
+var mongoose = require('mongoose');
+var mongodb = require('mongodb');
 
 exports.models = models = {};
 exports.schemas = schemas = {};
@@ -9,4 +12,37 @@ exports.schemas = schemas = {};
 //require('./post'),
 //require('./comment'),
 require('./user'),
-].forEach(_.partial(exportModel, module.exports));
+].forEach(_.partial(utils.exportModel, module.exports));
+
+function _extractTypeFromId(id) {
+  if (id instanceof mongodb.DBRef)
+    return id.namespace;
+  var index = id.indexOf('@', 1);
+  if (index > -1) {
+    var start = id.charAt(0) == '@' ? 1 : 0;
+    return id.slice(start, index);
+  } else {
+    return null;
+  }
+}
+
+/*
+* returns promise
+*/
+exports.findById = function(id){
+  if (!id)
+    return Promise.resolve(null);
+  try {
+    var type = _extractTypeFromId(id);
+  } catch (err) {}
+  if (!type)
+    return Promise.reject(new mongoose.Error('bad id: ' + id));
+  var model = mongoose.models[type];
+  if (!model) {
+    model = exports.models[type];
+    if (!model)
+      return Promise.reject(new mongoose.Error('missing model: ' + type));
+    model = mongoose.models[type];
+  }
+  return model.findOne({_id:id}).exec();
+}
